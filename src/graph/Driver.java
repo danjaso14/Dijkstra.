0db35c089b34727ed65687e191a1096669a66fe2
Djaso <djaso@dons.usfca.edu>
2018-04-18 20:52:46 -0700

    1st push: Includes HashTable class with insert and find methods first version (among other helper methods)

--- src/graph/Driver.java
+++ src/graph/Driver.java
@@ -6,13 +6,29 @@ import java.util.ArrayList;
 public class Driver {
     public static void main(String[] args) {
             // Initialize a graph
-            Graph graph = new Graph();
+//            Graph graph = new Graph();
+//
+//            // Create an instance of the Dijkstra class
+//            Dijkstra dijkstra = new Dijkstra("USA.txt", graph);
+//
+//            // Create a graphical user interface and wait for user to click
+//            // on two cities:
+//            GUIApp app = new GUIApp(dijkstra, graph);
 
-            // Create an instance of the Dijkstra class
-            Dijkstra dijkstra = new Dijkstra("USA.txt", graph);
+        HashTable h = new HashTable();
+        h.loadCities("USA.txt");
+        h.print();
+        System.out.println("\n id: " + h.find("Orlando"));
+        System.out.println("\n id: " + h.find("SanDiego"));
+        System.out.println("\n id: " + h.find("SanFrancisco"));
+        System.out.println("\n id: " + h.find("LosAngeles"));
+        System.out.println("\n id: " + h.find("Phoenix"));
 
-            // Create a graphical user interface and wait for user to click
-            // on two cities:
-            GUIApp app = new GUIApp(dijkstra, graph);
+
+
+
+
+
+
     }
 }
--- src/graph/HashTable.java
+++ src/graph/HashTable.java
@@ -1,10 +1,183 @@
 package graph;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+
+
+class LinkedHash
+{
+    String key;
+    int id;
+    LinkedHash next;
+
+
+    LinkedHash (String key, int id)
+    {
+        this.key = key;
+        this.id = id;
+        this.next = null;
+
+    }
+
+}
+
 /** Custom implementation of a hash table using open hashing, separate chaining.
  *  Each key is a String (name of the city), each value is an integer (node id). */
-public class HashTable {
+public class HashTable
+{
 	 // FILL IN CODE: add variables and methods
+
+
+    private int tableSize;
+    private int size;
+    private LinkedHash [] table;
+
+
+    public HashTable ()
+    {
+        size = 0;
+        tableSize = 20;
+        table = new LinkedHash[tableSize];
+
+        for(int i = 0; i < tableSize; i++)
+            table[i] = null;
+    }
+
+
+
+    public int myhash(String key)
+    {
+
+        int hashvalue = key.hashCode();
+        hashvalue = hashvalue % tableSize;
+
+        if(hashvalue < 0)
+        {
+            hashvalue = hashvalue + tableSize;
+        }
+
+        return hashvalue;
+
+
+    }
+
+    public void insert(String key, int id)
+    {
+
+
+        if(table[id] == null)
+        {
+            table[id] = new LinkedHash(key,id);
+        }
+
+        else
+        {
+            LinkedHash node = table[id];
+
+            while (node.next != null && !node.key.equals(key))
+            {
+                node = node.next;
+            }
+
+            if (node.key.equals(key))
+                node.key = key;
+
+            else
+                node.next = new LinkedHash(key, id);
+
+        }
+    }
+
+    public int find(String key)
+    {
+        int id = myhash(key) % tableSize;
+
+        if(table[id] == null)
+            return -1;
+
+        else
+        {
+            LinkedHash node = table[id];
+
+            while(node != null && !node.key.equals(key))
+            {
+                node = node.next;
+            }
+
+            if(node == null)
+                return -1;
+
+            else
+                return node.id;
+        }
+    }
+
+    public void print()
+    {
+        for (int i = 0; i < tableSize; i++)
+        {
+            System.out.print("\nBucket "+ (i) +" : ");
+            LinkedHash node = table[i];
+            while (node != null)
+            {
+                System.out.print(node.key +" ");
+                node = node.next;
+            }
+        }
+    }
+
+
+
+    public void loadCities(String File)
+    {
+
+        try (FileReader f = new FileReader(File);
+             BufferedReader br = new BufferedReader(f))
+        {
+            br.readLine();
+            tableSize = Integer.parseInt(br.readLine());
+            String line;
+
+
+            while ((line = br.readLine()) != null && size < tableSize)
+            {
+                size++;
+
+                String [] splitline = line.split(" ");
+
+
+
+                String key = splitline[0];
+                int id = myhash(key);
+                insert(key, id);
+
+
+
+            }
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
 
 
 
 }
+
+
+
+
+
+
+
+
+
